b97e93c75a20e31fe92242ffe522f87040f55b14
Ziyu Zhou <897224436@qq.com>
2021-04-13 21:28:54 -0400

    add vuepress project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  title: "Harry Potter",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  base: "/",
+  dest: "./dist",
+
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      { text: "Projects", link: "/projects/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "GitHub", link: "https://github.com/mtobeiyf/vuepress-homepage" }
+    ],
+    sidebar: {
+      '/guide/': genSidebarConfig('Guide')
+    },
+    lastUpdated: 'Last Updated'
+  },
+
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    extendMarkdown: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
+  ]
+}
+
